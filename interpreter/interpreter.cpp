5e51496ee34976477efeb4d840ddf372a7fe61d7
kotfind <kotfind@yandex.ru>
2024-02-25 18:54:36 +0300

    Read code from file

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -1,11 +1,39 @@
 #include "State.hpp"
-#include "commands.hpp" // TODO: delete me
 #include "parse.hpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-int main() {
-    auto [code, labels] = parse_code_file("PUSH 1 PUSH 2 ADD OUT");
+void usage(const char* exec_name) {
+    std::cerr
+        << "Usage:\n"
+        << exec_name << " file"
+        << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        usage(argv[0]);
+        return -1;
+    }
+
+    std::ifstream file(argv[1]);
+    std::string code_string;
+    if (file.is_open()) {
+        std::ostringstream ss;
+        ss << file.rdbuf();
+        code_string = ss.str();
+        file.close();
+    } else {
+        std::cerr << "cannot open file: " << argv[1] << std::endl;
+        return -1;
+    }
+
+    auto [code, labels] = parse_code_file(code_string);
+
     State state(code, labels);
     state.run();
+
     for (auto* command : code) {
         delete command;
     }
